619c3daf030d7a84bd93703aa9bded6bda0a5dad
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-14 19:28:50 +0600

    database connected

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjepcue.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        console.log('database connected');
+    }
+    finally{
+        await client.close();
+    }
+}run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Mobile Panda!');
