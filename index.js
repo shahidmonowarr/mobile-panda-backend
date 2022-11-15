6284310d8a889e5983dbd36679c1dd63e8fed153
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-15 16:14:34 +0600

    find one service with id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -24,10 +24,14 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);});
     }
-    finally{
-        
-    }
+    finally{}
 }run().catch(console.dir);
 
 app.get('/', (req, res) => {
