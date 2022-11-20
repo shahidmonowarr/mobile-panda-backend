cb0b39eb2f215df03f5cfb5b1a0e6bfdc89b5c47
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-20 22:45:15 +0600

    get using email

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,43 +11,56 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjepcue.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        await client.connect();
-        const serviceCollection = client.db("mobile_panda").collection("services");
-        const orderCollection = client.db("mobile_panda").collection("orders");
+  try {
+    await client.connect();
+    const serviceCollection = client.db("mobile_panda").collection("services");
+    const orderCollection = client.db("mobile_panda").collection("orders");
 
-        app.get('/service', async (req, res) => {
-            const cursor = serviceCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
-        });
+    app.get("/service", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        app.get('/service/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service);});
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.json(result);
-        });
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
 
-        app.get('/order', async (req, res) => {
-          const cursor = orderCollection.find({});
-          const orders = await cursor.toArray();
-          res.send(orders);
-      });
-    }
-    finally{}
-}run().catch(console.dir);
+    // app.get("/order", async (req, res) => {
+    //   const cursor = orderCollection.find({});
+    //   const orders = await cursor.toArray();
+    //   res.send(orders);
+    // });
 
-app.get('/', (req, res) => {
-  res.send('Hello Mobile Panda!');
+    app.get("/order", async (req, res) => {
+      const query = { email: req.query.email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Mobile Panda!");
 });
 
 app.listen(port, () => {
