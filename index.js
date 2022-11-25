48830e0b1c4aaad4e9ce608b9782664efbafe00f
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-25 23:54:53 +0600

    create api to make user an admin

--- index.js
+++ index.js
@@ -41,6 +41,21 @@ async function run() {
     const reviewCollection = client.db("mobile_panda").collection("reviews");
     const userCollection = client.db("mobile_panda").collection("users");
 
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {role: "admin"},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
